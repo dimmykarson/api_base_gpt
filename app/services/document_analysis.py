3ef69729f7153d8c99212aaf2a2637a3b01e7b35
Dimmy Magalhães <dimmyk@gmail.com>
2025-03-20 00:25:27 -0300

    Deepseek

--- app/services/document_analysis.py
+++ app/services/document_analysis.py
@@ -1,14 +1,34 @@
-from app.models.features import DocumentMetadata
+#from app.models.features import DocumentMetadata
 from langchain.document_loaders import PyPDFLoader
 from langchain.embeddings import HuggingFaceEmbeddings
-from langchain.llms import HuggingFaceHub
 import os
 from dotenv import load_dotenv
 import json
+from langchain_ollama import ChatOllama
+
+import asyncio, re
+
+#from app.models.features import DocumentMetadata
 
 load_dotenv()
 
-api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+llm = ChatOllama(
+    model='deepseek-r1:8b',
+)
+
+def extract_json_from_text(response_text):
+    # Expressão regular para capturar JSON dentro da resposta
+    match = re.search(r"\{.*?\}", response_text, re.DOTALL)
+    if match:
+        try:
+            json_data = json.loads(match.group())  # Tenta converter a string para um dicionário
+            return json_data
+        except json.JSONDecodeError:
+            print("Erro ao decodificar JSON")
+            return None
+    else:
+        print("Nenhum JSON encontrado na resposta")
+        return None
 
 def extract_document_metadata(file_path):
     loader = PyPDFLoader(file_path)
@@ -16,18 +36,10 @@ def extract_document_metadata(file_path):
     
     text = "\n".join([page.page_content for page in pages])
     
-    llm = HuggingFaceHub(repo_id="mistralai/Mistral-7B-Instruct-v0.1", huggingfacehub_api_token=api_token)
     
     prompt = f"""
-        Extraia as seguintes informações do texto do documento e retorne apenas um JSON válido:
-    {{
-    "title":"", 
-    "autor":"", 
-    "data":"", 
-    "resumo":"",
-    "key_words":""
-    }}
-
+        Extraia as seguintes informações do texto do documento e retorne apenas um JSON:
+    
     Instruções: 
     * Key Words são palavras chaves que resumem o conteúdo do texto.
     * Resumo é uma descrição curta do texto.
@@ -36,13 +48,24 @@ def extract_document_metadata(file_path):
     {text}
 
 
-    ####RESPOSTA####
+    Formato da resposta:
+    {{
+    "title":"", 
+    "autor":"", 
+    "data":"", 
+    "resumo":"",
+    "key_words":""
+    }}
+
     """
     
-    response = llm(prompt)
+    response = llm.invoke(prompt)
     
-    extracted_data = response.split("####RESPOSTA####")
-    extracted_data = extracted_data[1].strip()
-    extracted_data = json.loads(extracted_data)
-    print(":: Finalizado")
-    return extracted_data
+    return extract_json_from_text(response.content)
+
+
+if __name__ == "__main__":
+    file_path = "/home/dimmy/Projects/api_base_gpt/tests/exemplo.pdf"
+    metadata = extract_document_metadata(file_path)
+    #metadata = extract_json_from_text(metadata)
+    print(metadata)
